2f5c594772d160978c4f8fb4c2f9a2122a4ed8d8
li-yanhao <yanhao.li@outlook.com>
2022-09-15 18:31:53 +0200

    test

---
+++ run.sh
@@ -0,0 +1,14 @@
+
+export HOME=/home/ipol
+
+cd $HOME
+
+python main.py \
+    --mode test --cuda 0 \
+    --version ASMAfinal  \
+    --dataloader_workers 8   \
+    --testImgRoot ./bench/ \
+    --nodeName localhost \
+    --checkpoint 350000 \
+    --testScriptsName common_useage \
+    --specify_sytle 8 > stdout.txt
