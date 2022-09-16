fcb27af8f26bfdf70fced0188801283769810376
li-yanhao <yanhao.li@outlook.com>
2022-09-16 15:50:24 +0200

    test

--- run.sh
+++ run.sh
@@ -6,12 +6,12 @@ cd $HOME
 echo HELLO > stdout.txt
 
 
-# python main.py \
-#     --mode test --cuda 0 \
-#     --version ASMAfinal  \
-#     --dataloader_workers 8   \
-#     --testImgRoot ./bench/ \
-#     --nodeName localhost \
-#     --checkpoint 350000 \
-#     --testScriptsName common_useage \
-#     --specify_sytle 8  > stdout.txt
+python main.py \
+    --mode test --cuda 0 \
+    --version ASMAfinal  \
+    --dataloader_workers 8   \
+    --testImgRoot ./bench/ \
+    --nodeName localhost \
+    --checkpoint 350000 \
+    --testScriptsName common_useage \
+    --specify_sytle 8  >> stdout.txt
